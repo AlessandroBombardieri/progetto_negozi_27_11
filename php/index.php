b97828077b1de08b9be20727da401ebb9e2ae345
alessandro.bombardieri1 <alessandro.bombardieri1@studenti.unimi.it>
2025-11-28 14:39:16 +0100

    #

--- php/index.php
+++ php/index.php
@@ -1,29 +1,32 @@
 <?php
-    ini_set ("display_errors", "On");
-	ini_set("error_reporting", E_ALL);
-    session_start();
+ini_set("display_errors", "On");
+ini_set("error_reporting", E_ALL);
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/utils.css">
     <title>Pagin di login</title>
 </head>
+
 <body>
     <div class="container">
         <?php
-            if (isset($_SESSION['feedback'])){
-                if ($_SESSION['feedback'] == false){
-                    ?>
-                        <div class="alert alert-danger" role="alert">
-                            Credenziali errate, ritenta.
-                        </div>
-                    <?php
-                }
+        if (isset($_SESSION['feedback'])) {
+            if ($_SESSION['feedback'] == false) {
+                ?>
+                <div class="alert alert-danger" role="alert">
+                    Credenziali errate, ritenta.
+                </div>
+                <?php
             }
+        }
         ?>
         <div id="form_container">
             <form id="myform" method="POST" action="lib/login.php">
@@ -32,7 +35,8 @@
                 <br>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">E-mail</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email'>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        name='email'>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
@@ -43,6 +47,11 @@
         </div>
     </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+    crossorigin="anonymous"></script>
+
 </html>
