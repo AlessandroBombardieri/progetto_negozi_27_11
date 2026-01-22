27d5173092c7e09535de3fca8a434e59fbcefdab
alessandro.bombardieri1 <alessandro.bombardieri1@studenti.unimi.it>
2026-01-22 14:52:50 +0100

    .

--- php/conf/conf.php
+++ php/conf/conf.php
@@ -3,7 +3,7 @@
 /*
 define('DB_HOST', ''); //e.g. 127.0.0.1
 define('DB_PORT', ''); //e.g. 5432
-define('DB_NAME', ''); //e.g. catena_negozi
+define('DB_NAME', ''); //e.g. progettoNegozi
 define('DB_USER', ''); //e.g. bombardieri
 define('DB_PASS', ''); //e.g. password
 ?>
