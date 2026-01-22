f84a4c63c979ae7d3c00f60dbd45df1a28f32cfb
alessandro.bombardieri1 <alessandro.bombardieri1@studenti.unimi.it>
2026-01-22 11:11:55 +0100

    .

---
+++ sql/insert.sql
@@ -0,0 +1,7 @@
+/* Insert. */
+
+/* Manager. */
+insert into utente values('RSSGFR90D17L175V', 'gianfranco.rossi@gmail.com', 'gattopardo45', 'manager', 'gianfranco', 'rossi', 'sondrio', 'tirano', 'viale italia', 7);
+
+/* Cliente. */
+insert into utente values('FRRMRA00E15F205B', 'mauro.ferrari@gmail.com', 'milano88!', 'cliente', 'mauro', 'ferrari', 'milano', 'milano', 'celoria', 18);
