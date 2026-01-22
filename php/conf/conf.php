6a3196f0797da83119c49bc565ad7309614b636e
alessandro.bombardieri1 <alessandro.bombardieri1@studenti.unimi.it>
2026-01-22 14:44:38 +0100

    .

--- php/conf/conf.php
+++ php/conf/conf.php
@@ -1,5 +1,13 @@
 <?php
-/* Modificare i seguenti valori secondo il setup locale MAMP/Postgres */
+// Sostituire i seguenti valori di configurazione a seconda del proprio database locale PostgreSQL utilizzato.
+/*
+define('DB_HOST', ''); //e.g. 127.0.0.1
+define('DB_PORT', ''); //e.g. 5432
+define('DB_NAME', ''); //e.g. catena_negozi
+define('DB_USER', ''); //e.g. bombardieri
+define('DB_PASS', ''); //e.g. password
+?>
+*/
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '5432');
 define('DB_NAME', 'progetto_27_11_test');
